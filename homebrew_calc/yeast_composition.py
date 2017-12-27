3d92cbe10e4c282db9d612b29a0dae016c96b7b3
Bob Wilson <millstadtf@gmail.com>
2017-12-26 19:26:59 -0800

    Adding documentation.

--- homebrew_calc/yeast_composition.py
+++ homebrew_calc/yeast_composition.py
@@ -6,6 +6,9 @@ from unit_parser import unit_parser
 
 
 def abvcalc_main():
+    """Entry point for abvcalc command line script.
+
+    """
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('og', type=float, help='Original Gravity')
@@ -119,6 +122,9 @@ def deg_plato_to_gravity(deg_plato):
 
 
 def main():
+    """Entry point for yeast_composition command line script.
+
+    """
     import argparse
 
     this_dir, this_filename = os.path.split(__file__)
@@ -141,6 +147,55 @@ def main():
 
 
 def execute(config, recipe_config):
+    """Calculations relevant to yeast characteristics.
+
+    Note: required parameters are in either config or
+    recipe_config. Where applicable, if a parameter is specified in
+    both config and recipe_config, the latter overrides the former.
+
+    Parameters
+    ----------
+     'Yeast' : array_like
+        Array of one or more yeast packages. Each package is specified
+        by a collection of key-value pairs. See below.
+     'Original Gravity' : float
+        Specific gravity of wort before pitching yeast. This can
+        either be a top-level parameter, representing the predicted
+        original gravity from malt_composition, or the actual,
+        measured value as recorded, e.g., by brew_day. In the latter
+        case, the parameter should be a sub-parameter of 'Brew
+        Day'. If both are present, the 'Brew Day' parameter takes
+        precedence.
+     'Pitchable Volume' : str
+        String representing the final volume of wort in which yeast
+        will be pitched, e.g. '5 gallons'. Defaults to '5.25 gallons'
+        if missing from both recipe_config and config.
+     'Ale or Lager' : str
+        One of 'Ale' or 'Lager'. If not specified, defaults to 'Ale'.
+
+    Yeast Parameters
+    ----------------
+     'attenuation' : float
+        Attenuation of yeast. If multiple yeast strains are used, we
+        assume the overall attenuation is determined by the yeast with
+        the highest advertised attenuation.
+
+
+    Returns
+    -------
+     This function does not return anything. Instead, it appends the
+     following parameters to recipe_config, and if requested, saves
+     the latter to the specified file. It also prints the number of
+     yeast cells needed, which translates into how big of a starter to
+     use. Starter size calculations have not yet been implemented but
+     are very imortant!
+
+     'Final Gravity' : float
+        Predicted final gravity of beer.
+     'Alcohol by Volume' : float
+        Predicted final ABV of beer.
+
+    """
     # Viability
     # Starter calculation
 
@@ -163,23 +218,29 @@ def execute(config, recipe_config):
         print('Final Gravity: {0:.03f}'.format(fg))
         print('Alcohol by Volume: {0:.01f}%'.format(100. * abv))
 
+        if 'units' in config:
+            up = unit_parser(config['units'])
+        else:
+            up = unit_parser()
+
         if 'Pitchable Volume' in recipe_config:
-            # Want 750k cells per milliliter per degree Plato for ales
-            # 1.5 million for lagers
-            if recipe_config.get('Ale or Lager', 'Ale') == 'Ale':
-                cells_needed = 750000
-            else:
-                cells_needed = 1500000
+            pitchable_volume = up.convert(recipe_config['Pitchable Volume'], 'milliliters')
+        elif 'Pitchable Volume' in config:
+            pitchable_volume = up.convert(config['Pitchable Volume'], 'milliliters')
+        else:
+            pitchable_volume = up.convert('5.25 gallons', 'milliliters')
+            print('Pitchable volume not specified, assuming 5.25 gallons')
 
-            if 'units' in config:
-                up = unit_parser(config['units'])
-            else:
-                up = unit_parser()
+        # Want 750k cells per milliliter per degree Plato for ales
+        # 1.5 million for lagers
+        if recipe_config.get('Ale or Lager', 'Ale') == 'Ale':
+            cells_needed = 750000
+        else:
+            cells_needed = 1500000
 
-            pv = up.convert(recipe_config['Pitchable Volume'], 'milli_liters')
-            degP = gravity_to_deg_plato(og)
-            cell_count = cells_needed * pv * degP
-            print('Cells needed (billions): {0:.0f}'.format(cell_count / 1e9))
+        degP = gravity_to_deg_plato(og)
+        cell_count = cells_needed * pitchable_volume * degP
+        print('Cells needed (billions): {0:.0f}'.format(cell_count / 1e9))
 
     if 'Output' in config:
         with open(config['Output'], 'w') as outfile:
